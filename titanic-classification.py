f4701837965e156c20aab5c1161609192cca75b3
yyan486 <yyan486@emory.edu>
2020-11-18 19:46:18 -0500

    part 1 update python code

--- titanic-classification.py
+++ titanic-classification.py
@@ -30,12 +30,12 @@ def feature_preprocess(data_in):
     data.loc[data["Sex"] == "male", "Sex"] = 0
     data.loc[data["Sex"] == "female", "Sex"] = 1
     data["Embarked"] = data["Embarked"].fillna(data["Embarked"].median())
-    
+
     data["Age"] /= data["Age"].max()
     data["Embarked"] /= data["Embarked"].max()
     data["Pclass"] /= data["Pclass"].max()
     data["Sex"] /= data["Sex"].max()
-    
+
     return data
 
 
@@ -68,7 +68,7 @@ x_test = extract_feature(y)
 class LogisticRegression:
     def __init__(self, threshold=0.5):
         self.threshold = threshold
-        
+
     def predict(self, x):
         z = np.sum(x * self.W, axis=1)
         return 1 / (1 + np.exp(-z))
@@ -77,14 +77,14 @@ class LogisticRegression:
         y_hat = self.predict(x)
         new_W = np.matmul(x.T, y - y_hat)
         self.W = self.W + learning_rate * new_W
-        
+
     def evaluate(self, x, y):
         y_pred = self.predict(x)
         return {
             "loss": -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred)),
             "accuracy": np.sum((y_pred > self.threshold).astype(int) == y) / y.shape[0]
         }
-    
+
     def fit(
         self, x, y, x_valid = None, y_valid = None,
         learning_rate = 0.001,
@@ -114,19 +114,22 @@ class LogisticRegression:
                 if (e <= 5 or (i + 1) == step) and verbose:
                     metrics = self.evaluate(x_valid, y_valid)
                     print("Epoch %d Step %d: Loss %f, Acc %f" % (e + 1, i + 1, metrics["loss"], metrics["accuracy"]))
-            
+
             metrics = self.evaluate(x_valid, y_valid)
             metric_graph["loss"].append(metrics["loss"])
             metric_graph["accuracy"].append(metrics["accuracy"])
             learning_rate *= learning_rate_decay
-        
-        plt.plot(metric_graph["loss"])
-        plt.title("Loss")
-        plt.show()
+
+        #plt.plot(metric_graph["loss"])
+        #plt.title("Loss")
+        #plt.show()
         plt.title("Accuracy")
+        plt.xlabel("EPOCH")
+        plt.ylabel("ACCURACY")
         plt.plot(metric_graph["accuracy"])
+        plt.savefig("accuracy.png")
         plt.show()
-        
+
 
 
 # In[25]:
@@ -141,9 +144,9 @@ print("Data Size: %d\nTrain Size: %d" % (x_train.shape[0], split_size))
 
 model = LogisticRegression(threshold=threshold)
 model.fit(
-    x_train[0:split_size], y_train[0:split_size], 
-    x_valid = x_train[split_size:], y_valid = y_train[split_size:], 
-    learning_rate=learning_rate, 
+    x_train[0:split_size], y_train[0:split_size],
+    x_valid = x_train[split_size:], y_valid = y_train[split_size:],
+    learning_rate=learning_rate,
     batch_size=batch_size,
     epoch=epoch
 )
@@ -157,7 +160,7 @@ print("Validate", model.evaluate(x_train[split_size:], y_train[split_size:]))
 final_model = LogisticRegression(threshold=threshold)
 final_model.fit(
     x_train, y_train,
-    learning_rate=learning_rate, 
+    learning_rate=learning_rate,
     batch_size=batch_size,
     epoch=epoch
 )
@@ -165,4 +168,3 @@ y_out = (model.predict(x_test) > threshold).astype(int)
 output = pd.DataFrame(test["PassengerId"])
 output["Survived"] = y_out
 output.to_csv("output.txt", index=False)
-
